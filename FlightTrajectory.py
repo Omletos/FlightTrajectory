62d0cb2eed05f7d41766d21eb0d64521399fcfd1
Omletos <93455802+Omletos@users.noreply.github.com>
2026-01-31 17:45:57 +1300

    Add project files.

---
+++ FlightTrajectory.py
@@ -0,0 +1,12 @@
+WET_MASS = 10 #kg
+FUEL_MASS = 9 #kg
+BURN_TIME = 15 #sec
+THRUST = 100 #N
+AREA = 0.008 #m^2
+DRAG_COEF = 0.3
+#Launch angle 
+T_STEP = 10 #sec
+I_VELOCITY = 0 #m/s
+
+def atmospheric_density(h):
+    temp = 
